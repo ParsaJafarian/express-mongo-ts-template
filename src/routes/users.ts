a63d2c3bf1c59a03d59ef2d5d89e77da4c747e03
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-04-27 14:22:14 -0400

    Add users route

---
+++ src/routes/users.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import userController from '../controllers/users';
+
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUser);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+export default router;
